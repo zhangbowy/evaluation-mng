c582cb6480121c6f2cf5a6ab4b4b80dfc89d977a
zhangruofan <zrf@codeboxes.cn>
2022-03-30 19:45:27 +0800

    增加配置文件

---
+++ config/config.daily.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  dynamicImport: false,
+  base: '/admin/',
+  publicPath: '//daily-qzz-static.forwe.store/evaluation-mng/static/',
+});
