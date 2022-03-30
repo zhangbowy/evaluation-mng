7a09bf9bd7d57a1d61b046af8f9246eaa20f37ac
zhangruofan <zrf@codeboxes.cn>
2022-03-30 20:28:45 +0800

    1

---
+++ src/pages/user/login1/index.tsx
@@ -0,0 +1,56 @@
+import { PageLoading } from '@ant-design/pro-layout';
+import { useEffect } from 'react';
+import dd from 'dingtalk-jsapi';
+import { message } from 'antd';
+import { login } from '@/services/api';
+import queryString from 'query-string';
+import { history, useModel } from 'umi';
+
+const Login: React.FC = () => {
+  const { initialState, setInitialState } = useModel('@@initialState');
+  const { corpId, appId, clientId } = queryString.parse(location.hash) as {
+    corpId: string;
+    appId: string;
+    clientId: string;
+  };
+  useEffect(() => {
+    if (!corpId) {
+      message.error('缺少企业id');
+      return;
+    }
+    dd.ready(async () => {
+      const result = await dd.runtime.permission.requestAuthCode({ corpId });
+
+      const res = await login({ code: result.code, corpId, appId });
+
+      if (res.code === 1) {
+        if (!res.data.authLogin) {
+          if (!clientId) {
+            message.error('缺少clientId');
+            return;
+          }
+          // 未授权登录需要先授权登录
+          window.location.replace(
+            `https://login.dingtalk.com/oauth2/auth?redirect_uri=${encodeURIComponent(
+              `https://${window.location.hostname}${window.location.search}#/user/callback`,
+            )}&response_type=code&client_id=${clientId}&scope=openid&prompt=consent`,
+          );
+        } else {
+          // 已经授权则免登进入系统
+          setInitialState({
+            ...initialState,
+            user: res.data.user,
+          });
+          window.sessionStorage.setItem('QAT', res.data.token);
+          history.replace('/exam/template');
+        }
+      } else {
+        // 免登失败，提示进入403
+        history.replace('/403');
+      }
+    });
+  }, [corpId, appId, clientId, setInitialState, initialState]);
+  return <PageLoading />;
+};
+
+export default Login;
