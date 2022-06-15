7f0a1a9a8ba67c516ca8f291eedc9284c7bea12a
马文卿 <529837505@qq.com>
2022-06-15 11:34:24 +0800

    commit

--- config/routes.ts
+++ config/routes.ts
@@ -7,6 +7,13 @@
     component: './403',
     hideInMenu: true,
   },
+  {
+    path: '/402',
+    name: '功能预览',
+    layout: false,
+    component: './402',
+    hideInMenu: true,
+  },
   { path: '/exam/template', icon: 'reconciliation', name: '测评库', component: './exam/template' },
   { path: '/exam/index', icon: 'appstore', name: '测评管理', component: './exam' },
   { path: '/exam/:id', hideInMenu: true, name: '测评详情', component: './exam/detail' },
