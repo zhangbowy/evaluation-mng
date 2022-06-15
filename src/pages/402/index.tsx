aaf8a2a649ac231497958c1f0210f1166b268480
马文卿 <529837505@qq.com>
2022-06-15 11:31:00 +0800

    commit

---
+++ src/pages/402/index.tsx
@@ -0,0 +1,41 @@
+import { openTryoutSku } from 'dingtalk-design-libs';
+import React, { FC, Fragment, useEffect } from 'react';
+import { history } from 'umi';
+import queryString from 'query-string';
+import styles from "./index.less"
+
+const PreviewPage: FC = () => {
+    const { corpId, appId, purchaseToken } = queryString.parse(location.search) as {
+        corpId: string;
+        appId: string;
+        purchaseToken: string;
+    };
+
+    const handleSku = () => {
+        openTryoutSku({
+            corpId: corpId,
+            appId: Number(appId),
+            token: '',
+        }).then((res: any) => {
+            const { action } = res;
+            if (action === "ok") {
+                history.replace('/user/login');
+            }
+        }).catch((err: any) => {
+            // 钉钉侧出现了技术异常，比如打开弹窗失败等，出现概率非常低
+            console.log(err);
+        });
+    };
+
+    useEffect(() => {
+        handleSku();
+    }, []);
+
+    return (
+        <Fragment>
+            <img className={styles.preview_img} onClick={handleSku} src="//qzz-static.forwe.store/evaluation-mng/imgs/%E8%B6%A3%E6%B5%8B%E8%AF%84logo2.png" alt="" />
+        </ Fragment>
+    )
+}
+
+export default PreviewPage;
