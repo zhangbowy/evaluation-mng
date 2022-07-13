f625d7b22bfd0891e387d496916bc8a4d153e68e
dingtalk_afdlwy <lh995412029@163.com>
2022-07-13 14:54:54 +0800

    修改

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -19,57 +19,37 @@ const Login: React.FC = () => {
       message.error('缺少企业id');
       return;
     }
-    console.log(corpId, appId, clientId, 'console.log(corpId && appId && clientId)')
-    console.log(location.href, 'location.href')
-    console.log(dd.env.platform)
-    console.log(dd.env.platform != 'notInDingTalk')
     if (corpId && appId && clientId) {
-      try {
-        dd.env.platform != 'notInDingTalk' && dd.ready(async () => {
-          console.log('dd.ready进来了')
-          const result = await dd.runtime.permission.requestAuthCode({
-            corpId,
-            onSuccess: async (res: any) => {
-              console.log(res, '免登授权码res')
-            },
-            onFail: function (err: any) {
-              console.log(err, '免登授权码err')
+      dd.env.platform != 'notInDingTalk' && dd.ready(async () => {
+        const result = await dd.runtime.permission.requestAuthCode({ corpId });
+        const res = await login({ code: result.code, corpId, appId });
+        if (res.code === 1) {
+          if (!res.data.authLogin) {
+            if (!clientId) {
+              message.error('缺少clientId');
+              return;
             }
-          })
-          console.log(result, 'result')
-          const res = await login({ code: result.code, corpId, appId });
-          console.log(res, 'res')
-          if (res.code === 1) {
-            if (!res.data.authLogin) {
-              if (!clientId) {
-                message.error('缺少clientId');
-                return;
-              }
-              // 未授权登录需要先授权登录
-              window.location.replace(
-                `https://login.dingtalk.com/oauth2/auth?redirect_uri=${encodeURIComponent(
-                  `${window.location.origin}/admin${window.location.search}#/user/login/callback`,
-                )}&response_type=code&client_id=${clientId}&scope=openid&prompt=consent`,
-              );
-            } else {
-              console.log('已授权123123123')
-              // 已经授权则免登进入系统
-              setInitialState({
-                ...initialState,
-                user: res.data.user,
-              });
-              window.sessionStorage.setItem('QAT', res.data.token);
-              window.sessionStorage.setItem('QCP_User', JSON.stringify(res.data.user));
-              history.push('/exam/template');
-            }
+            // 未授权登录需要先授权登录
+            window.location.replace(
+              `https://login.dingtalk.com/oauth2/auth?redirect_uri=${encodeURIComponent(
+                `${window.location.origin}/admin${window.location.search}#/user/login/callback`,
+              )}&response_type=code&client_id=${clientId}&scope=openid&prompt=consent`,
+            );
           } else {
-            // 免登失败，提示进入403
-            history.push(`/403/${res.code}`);
+            // 已经授权则免登进入系统
+            setInitialState({
+              ...initialState,
+              user: res.data.user,
+            });
+            window.sessionStorage.setItem('QAT', res.data.token);
+            window.sessionStorage.setItem('QCP_User', JSON.stringify(res.data.user));
+            history.push('/exam/template');
           }
-        });
-      } catch (e) {
-        console.log(e, '错误了')
-      }
+        } else {
+          // 免登失败，提示进入403
+          history.push(`/403/${res.code}`);
+        }
+      });
     }
   }, [corpId, appId, clientId]);
   return <PageLoading tips="登录中" />;
