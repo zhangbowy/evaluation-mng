f1d18cbcbfd81ccbf73bb49445685ed7a47ad824
dingtalk_afdlwy <lh995412029@163.com>
2022-07-13 14:03:16 +0800

    修改

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -19,38 +19,41 @@ const Login: React.FC = () => {
       message.error('缺少企业id');
       return;
     }
-    console.log(corpId && appId && clientId, 'console.log(corpId && appId && clientId)')
+    console.log(corpId, appId, clientId, 'console.log(corpId && appId && clientId)')
+    console.log(location.href, 'location.href')
     if (corpId && appId && clientId) {
-      dd.ready(async () => {
-        const result = await dd.runtime.permission.requestAuthCode({ corpId });
-        console.log(result, 'result')
-        const res = await login({ code: result.code, corpId, appId });
-        if (res.code === 1) {
-          if (!res.data.authLogin) {
-            if (!clientId) {
-              message.error('缺少clientId');
-              return;
+      dd.ready(() => {
+        dd.runtime.permission.requestAuthCode({ corpId }).then(async result => {
+          const res = await login({ code: result.code, corpId, appId });
+          if (res.code === 1) {
+            if (!res.data.authLogin) {
+              if (!clientId) {
+                message.error('缺少clientId');
+                return;
+              }
+              // 未授权登录需要先授权登录
+              window.location.replace(
+                `https://login.dingtalk.com/oauth2/auth?redirect_uri=${encodeURIComponent(
+                  `${window.location.origin}/admin${window.location.search}#/user/login/callback`,
+                )}&response_type=code&client_id=${clientId}&scope=openid&prompt=consent`,
+              );
+            } else {
+              // 已经授权则免登进入系统
+              setInitialState({
+                ...initialState,
+                user: res.data.user,
+              });
+              window.sessionStorage.setItem('QAT', res.data.token);
+              window.sessionStorage.setItem('QCP_User', JSON.stringify(res.data.user));
+              history.push('/exam/template');
             }
-            // 未授权登录需要先授权登录
-            window.location.replace(
-              `https://login.dingtalk.com/oauth2/auth?redirect_uri=${encodeURIComponent(
-                `${window.location.origin}/admin${window.location.search}#/user/login/callback`,
-              )}&response_type=code&client_id=${clientId}&scope=openid&prompt=consent`,
-            );
           } else {
-            // 已经授权则免登进入系统
-            setInitialState({
-              ...initialState,
-              user: res.data.user,
-            });
-            window.sessionStorage.setItem('QAT', res.data.token);
-            window.sessionStorage.setItem('QCP_User', JSON.stringify(res.data.user));
-            history.push('/exam/template');
+            // 免登失败，提示进入403
+            history.push(`/403/${res.code}`);
           }
-        } else {
-          // 免登失败，提示进入403
-          history.push(`/403/${res.code}`);
-        }
+        }).catch(err => {
+          console.log(err, 'dd.runtime.permission.requestAuthCode')
+        })
       });
     }
   }, [corpId, appId, clientId]);
