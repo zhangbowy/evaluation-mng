adc831ab556976469fdbcbeea57cd989fd943919
tanwei <2534234979@qq.com>
2022-07-26 17:00:31 +0800

    fix:图片域名修改

--- src/components/report/MBTI/index.tsx
+++ src/components/report/MBTI/index.tsx
@@ -162,7 +162,7 @@ const PdfDetailMBTI = memo(forwardRef((props: any, ref)=>{
                         1942年，瑞士精神分析学家荣格第一次提出人格分类的概念。他认为感知和判断是大脑的两大基本功能。不同的人，感知倾向不同——有些人更侧重直觉，有些更侧重感觉。同样，不同的人判断倾向也不同——有些更倾向理性分析得出结论，有些更侧重情感，更为感性。同时，这两大基本功能又受到精力来源不同（内向或外向）的影响。以荣格的人格分类理论为基础，美国心理学家Katherine Cook Briggs(1875-1968)和Isabel Briggs Myers母女提出了影响大脑做出决定的第四因素，行为和生活方式；并综合荣格的人格分类学说形成MBTI人格模型。
                     </div>
                     <div className="picture">
-                        <img src={`${delPicDomain('https://qzz-static.forwe.store/public-assets/measurement-type.png?x-oss-process=image/resize,m_fill,w_282,h_254')}`} alt=""/>
+                        <img src={`${delPicDomain('https://qzz-static.forwe.store')}/public-assets/measurement-type.png?x-oss-process=image/resize,m_fill,w_282,h_254`} alt=""/>
                     </div>
                 </article>
                 <article className="article-text">
@@ -550,8 +550,8 @@ const PdfDetailMBTI = memo(forwardRef((props: any, ref)=>{
                             <div className="line ability-color-9"></div>
                         </div>
                         <div className="left-arrow">
-                            <img src={`${delPicDomain('https://qzz-static.forwe.store/evaluation-mng/imgs/qcp_pdf_up.png?x-oss-process=image/resize,m_fill,w_21,h_41')}`} />
-                            <img src={`${delPicDomain('https://qzz-static.forwe.store/evaluation-mng/imgs/qcp_pdf_down.png?x-oss-process=image/resize,m_fill,w_21,h_41')}`} />
+                            <img src={`${delPicDomain('https://qzz-static.forwe.store')}/evaluation-mng/imgs/qcp_pdf_up.png?x-oss-process=image/resize,m_fill,w_21,h_41`} />
+                            <img src={`${delPicDomain('https://qzz-static.forwe.store')}/evaluation-mng/imgs/qcp_pdf_down.png?x-oss-process=image/resize,m_fill,w_21,h_41`} />
                         </div>
                     </div>
                 </div>
@@ -853,7 +853,7 @@ const PdfDetailMBTI = memo(forwardRef((props: any, ref)=>{
                         <p>4、个人优劣势分析</p>
                     </div>
                     <div className="classify-image m-b-25">
-                        <img src={delPicDomain('https://qzz-static.forwe.store/evaluation-mng/imgs/qcp_pdf_superiority.png?x-oss-process=image/resize,m_fill,w_90,h_31')} />
+                        <img src={`${delPicDomain('https://qzz-static.forwe.store')}/evaluation-mng/imgs/qcp_pdf_superiority.png?x-oss-process=image/resize,m_fill,w_90,h_31`} />
                         <div className="line"></div>
                     </div>
                     <p className="abstract">
@@ -867,7 +867,7 @@ const PdfDetailMBTI = memo(forwardRef((props: any, ref)=>{
                         }
                     </div>
                     <div className="classify-image m-b-25">
-                        <img src={delPicDomain('https://qzz-static.forwe.store/evaluation-mng/imgs/qcp_pdf_inferiority.png?x-oss-process=image/resize,m_fill,w_90,h_31')} />
+                        <img src={`${delPicDomain('https://qzz-static.forwe.store')}/evaluation-mng/imgs/qcp_pdf_inferiority.png?x-oss-process=image/resize,m_fill,w_90,h_31`} />
                         <div className="line"></div>
                     </div>
                     <p className="abstract">
@@ -1000,7 +1000,7 @@ const PdfDetailMBTI = memo(forwardRef((props: any, ref)=>{
                             }
                         </div>
                         <div className="right">
-                            <img src={delPicDomain('https://qzz-static.forwe.store/evaluation-mng/imgs/qcp_pdf_warn.png?x-oss-process=image/resize,m_fill,w_91,h_91')} alt="警告" />
+                            <img src={`${delPicDomain('https://qzz-static.forwe.store')}/evaluation-mng/imgs/qcp_pdf_warn.png?x-oss-process=image/resize,m_fill,w_91,h_91`} alt="警告" />
                         </div>
                     </div>
                 </div>
