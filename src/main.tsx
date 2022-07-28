9ccb7da9e8452f549c2261312604648ea5b636fa
dingtalk_afdlwy <lh995412029@163.com>
2022-07-28 11:56:50 +0800

    build:修改配置文件

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import App from './app'
 import './assets/global.less'
 import zhCN from 'antd/lib/locale/zh_CN';
 import { ConfigProvider } from 'antd'
+// import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ConfigProvider>
+  <ConfigProvider locale={zhCN}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ConfigProvider>
   // </React.StrictMode>
 )
