144c01fe0f9dabb6a5e343dda7f417a32a6df258
dingtalk_afdlwy <lh995412029@163.com>
2022-11-15 14:33:23 +0800

    🐳 chore: 根据环境判断是否显示调试器

--- src/main.tsx
+++ src/main.tsx
@@ -16,4 +16,19 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </HashRouter>
   </ConfigProvider>
   // </React.StrictMode>
-)
+);
+
+(() => {
+  if (!location!.href.includes('qzz-eval')) {
+    const script1 = document.createElement('script');
+    script1.src = "//qzz-static.forwe.store/public-assets/eruda.js";
+    document.documentElement.appendChild(script1);
+    script1.onload = function () {
+      const script2 = document.createElement('script');
+      script2.text = "eruda.init()";
+      document.documentElement.appendChild(script2);
+    }
+  }
+})()
+
+
